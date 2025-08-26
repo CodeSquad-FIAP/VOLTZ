22aa5348a44391dc7c7f85fadeccd927a36edb0f
Arthur Cei Corrêa <126187274+arthurceicorrea@users.noreply.github.com>
2025-08-26 14:08:35 -0300

    Update User.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,37 +1,24 @@
 package model;
+
 public class User extends Entity {
-  private int id;
-  private String email;
-  private String password;
+    private String email;
+    private String password;
 
-  public User(String name, int id, String email, String password) {
-    super(name);
-    this.id = id;
-    this.email = email;
-    this.password = password;
-  }
+    public User(String name, String email, String password) {
+        super(name);
+        this.email = email;
+        this.password = password;
+    }
 
-  public int getId() {
-    return id;
-  }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-  public String getEmail() {
-    return email;
-  }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
-  public String getPassword() {
-    return password;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public void showInfo() {
-    System.out.println("User: " + name + ", Email: " + email);
-  }
+    @Override
+    public void showInfo() {
+        System.out.println("User: " + getName() + " | Email: " + email);
+    }
 }
+
